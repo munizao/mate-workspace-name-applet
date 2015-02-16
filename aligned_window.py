6a94f98311e36d1214174eb87e1a67afea26a8c1
munizao <pz@puzzlezapper.com>
2015-02-15 22:51:00 -0800

    Importing applet into git.

---
+++ aligned_window.py
@@ -0,0 +1,74 @@
+#Borrowed from Seth Nickell's gnome-blog applet
+#Not actually using this currently, because of stuff being broken in gdk 2.x using glib introspection, which isn't even supposed to work, but Mate requires it. 
+#import gtk
+import gi
+gi.require_version("Gtk", "2.0")
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import MatePanelApplet
+
+class AlignedWindow(Gtk.Window):
+
+    def __init__(self, applet):
+        Gtk.Window.__init__(self, Gtk.WindowType.TOPLEVEL)
+        self.applet = applet
+        self.alignment = self.applet.get_orient()
+
+	# Skip the taskbar, and the pager, stick and stay on top
+        self.set_decorated(False)
+        self.set_type_hint(Gdk.WindowTypeHint.DOCK)
+	self.stick()
+        self.set_resizable(False)
+        print "got through alignedwindow init"
+
+
+    def positionWindow(self):
+        # Get our own dimensions & position
+        self.realize()
+        Gdk.flush()
+        #print self.window.get_geometry()
+        #win = self.get_window()
+        print "got here pw 1"
+        #ought to work when we move to gtk3 and the bindings are unbroken
+        # or if I port to gnome flashback
+        #ourWidth  = (win.get_geometry())[2]
+        #ourHeight = (win.get_geometry())[3]
+        (ourWidth, ourHeight) = self.get_size()
+        # Get the dimensions/position of the applet
+        self.applet.realize()
+	#entryX, entryY = self.widgetToAlignWith.window.get_origin()
+        #entryWidth  = (self.widgetToAlignWith.window.get_geometry())[2]
+        #entryHeight = (self.widgetToAlignWith.window.get_geometry())[3]
+        print "got here pw 2"
+        #screen = self.applet.get_screen()
+        #appletWindow = self.applet.get_window()
+        monitor = Gdk.Rectangle(0, 0, 0, 0)
+        print "got here pw 2.1"
+        appletX = appletY = 0
+        (appletX, appletY) = self.applet.window.get_origin()
+        print "got here pw 2.2"
+        (appletWidth, appletHeight) = self.applet.window.get_size()
+        #screen.get_monitor_geometry (screen.get_monitor_at_window (self.applet.window), monitor)
+        print "got_here pw 3"
+        # Get the screen dimensions
+        screenHeight = Gdk.screen_height()
+        screenWidth = Gdk.screen_width()
+
+        if appletX + ourWidth < screenWidth:
+            # Align to the left of the entry
+            newX = appletX
+        else:
+            # Align to the right of the entry
+            newX = (appletX + appletWidth) - ourWidth
+
+        if appletY + appletHeight + ourHeight < screenHeight:
+            # Align to the bottom of the entry
+            newY = appletY + appletHeight
+        else:
+            newY = appletY - ourHeight
+
+        # -"Coordinates locked in captain."
+        # -"Engage."
+        self.move(newX, newY)
+        self.show()
+
---
+++ wsname_applet.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Uncomment to debug. If you aren't me, I bet you want to change the paths, too.
+#import os
+import sys
+sys.stdout = open ("/home/munizao/hacks/googlesvn/wsnamelet/debug.stdout", "w", 0)
+sys.stderr = open ("/home/munizao/hacks/googlesvn/wsnamelet/debug.stderr", "w", 0)
+
+print "got anything"
+
+import gi
+gi.require_version("Gtk", "2.0")
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import GObject
+from gi.repository import Pango
+from gi.repository import MatePanelApplet
+#gi.module.gi.require_version ("Wnck", "1.0")
+gi.require_version ("Wnck", "1.0")
+from gi.repository import Wnck
+from gi.repository import Gio
+
+from wsnamelet import wsnamelet_globals
+
+ 
+print "got past imports"
+
+
+#Internationalize
+import locale
+import gettext
+gettext.bindtextdomain('wsnamelet', '/usr/share/locale')
+gettext.textdomain('wsnamelet')
+locale.bindtextdomain('wsnamelet', '/usr/share/locale')
+locale.textdomain('wsnamelet')
+gettext.install('wsnamelet', '/usr/share/locale', unicode=1)
+
+#screen = None
+
+class WSNamePrefs(object):    
+    def __init__(self, applet):
+        print "got to prefs"
+        self.applet = applet
+        self.dialog = Gtk.Dialog("Workspace Name Applet Preferences",
+                                 None,
+                                 Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                                 (Gtk.STOCK_CLOSE, Gtk.ResponseType.CLOSE))
+        self.dialog.set_border_width(10)
+        width_spin_label = Gtk.Label(label=_("Applet width in pixels:"))
+        width_adj = Gtk.Adjustment(lower=30, upper=500, step_incr=1)
+        self.width_spin_button = Gtk.SpinButton.new(width_adj, 0.0, 0)
+        self.applet.settings.bind("width", self.width_spin_button, "value", Gio.SettingsBindFlags.DEFAULT)
+        width_spin_hbox = Gtk.HBox()
+        width_spin_hbox.pack_start(width_spin_label, True, True, 0)
+        width_spin_hbox.pack_start(self.width_spin_button, True, True, 0)
+        self.dialog.vbox.add(width_spin_hbox)
+        
+
+class WSNameEntry(Gtk.Entry):
+    def __init__(self, applet):
+        Gtk.Widget.__init__(self)
+        self.connect("activate", self._on_activate)
+        self.connect("key-release-event", self._on_key_release)
+        self.applet = applet
+
+    def _on_activate(self, event):
+        text = self.get_text()
+        self.applet.workspace.change_name(text)
+        self.applet.label.set_text(text)
+        self.applet.exit_editing()
+
+    def _on_key_release(self, widget, event):
+        if event.keyval == Gdk.KEY_Escape:
+            self.applet.exit_editing()
+
+
+class WSNameApplet(MatePanelApplet.Applet):
+    _name_change_handler_id = None
+    workspace = None
+    settings = None
+    prefs = None
+    width = 100
+    #auto_resize = False    
+    editing = False
+
+
+    
+    def __init__(self, applet):
+        print "Applet init"
+        self.applet = applet;
+        menuxml = """
+        <menuitem name="Prefs" action="Prefs" />
+        <menuitem name="About" action="About" />
+ 
+        """
+
+        actions = [("Prefs", Gtk.STOCK_PREFERENCES, "Preferences", None, None, self._display_prefs), 
+                   ("About", Gtk.STOCK_ABOUT, "About", None, None, self._display_about)]
+        actiongroup = Gtk.ActionGroup.new("WsnameActions")
+        actiongroup.add_actions(actions, None)
+        print "actions added"
+        applet.setup_menu(menuxml, actiongroup)
+        print "menu is setup"
+        self.init()
+        #self.__gobject_init__()
+
+
+
+    def _display_about(self, action):
+        print "_display_about"
+        about = Gtk.AboutDialog()
+        about.set_program_name("Workspace Name Applet")
+        about.set_version(wsnamelet_globals.version)
+        about.set_copyright("© 2006 - 2015 Alexandre Muñiz")
+        about.set_comments("View and change the name of the current workspace.\n\nTo change the workspace name, click on the applet, type the new name, and press Enter.")
+	about.connect ("response", lambda self, *args: self.destroy ())
+        about.show_all()
+
+    def _display_prefs(self, action):
+        self.prefs.dialog.show_all()
+        self.prefs.dialog.run()
+        self.prefs.dialog.hide()
+        
+
+    def set_width(self, width):
+        self.width = width
+        self.button.set_size_request(width, -1)
+        self.button.queue_resize()
+        self.entry.set_size_request(width, -1)
+        self.entry.queue_resize()
+
+    def on_width_changed(self, settings, key):
+        width = settings.get_int(key)
+        self.set_width(width)
+
+    def init(self):
+        print "init"
+        self.button = Gtk.Button()
+        self.button.connect("button-press-event", self._on_button_press)
+        self.button.connect("button-release-event", self._on_button_release)
+	self.label = Gtk.Label()
+        self.label.set_ellipsize(Pango.EllipsizeMode.END)
+	self.applet.add(self.button)
+	self.button.add(self.label)
+        self.entry = WSNameEntry(self)
+        self.entry.connect("button-press-event", self._on_entry_button_press)
+        try:
+            self.settings = Gio.Settings.new("com.puzzleapper.wsname-applet-py")
+            self.set_width(self.settings.get_int("width"))
+            self.settings.connect("changed::width", self.on_width_changed)
+        except:
+            print "Settings failed"
+            self.set_width(100)
+	print "we have settings"
+
+
+	self.screen = Wnck.Screen.get_default()
+        self.workspace = really_get_active_workspace(self.screen)
+	self.screen.connect("active_workspace_changed", self._on_workspace_changed)
+
+	self._name_change_handler_id = None
+
+        self.tooltips = Gtk.Tooltips()
+        self.tooltips.set_tip(self.button, _("Click to change the name of the current workspace"))
+
+        self.prefs = WSNamePrefs(self)
+        self.show_workspace_name()
+        self.applet.show_all()
+
+	return True	    
+
+    def _on_button_press(self, button, event, data=None):
+        print "got clicked"
+        print event.button
+        if event.button != 1:
+            button.stop_emission("button-press-event")
+
+    def _on_button_release(self, button, event, data=None):
+        if event.type == Gdk.EventType.BUTTON_RELEASE and event.button == 1:
+            self.editing = True
+            self.applet.remove(self.button)
+            self.applet.add(self.entry)
+            self.entry.set_text(self.workspace.get_name())
+            self.entry.set_position(-1)            
+            self.entry.select_region(0, -1)
+            self.applet.request_focus(event.time)
+            GObject.timeout_add(0, self.entry.grab_focus)
+            self.applet.show_all()
+
+    def _on_entry_button_press(self, entry, event, data=None):
+        self.applet.request_focus(event.time)
+
+    def _on_workspace_changed(self, event, old_workspace):
+        if self.editing:
+            self.exit_editing()
+	if (self._name_change_handler_id):
+	    self.workspace.disconnect(self._name_change_handler_id)
+        self.workspace = really_get_active_workspace(self.screen)
+	self._name_change_handler_id = self.workspace.connect("name-changed", self._on_workspace_name_changed)
+        self.show_workspace_name()
+
+    def _on_workspace_name_changed(self, event):
+	self.show_workspace_name()
+
+    def show_workspace_name(self):
+        print "got to show_workspace_name"
+        if self.workspace:
+            self.label.set_text(self.workspace.get_name())
+	self.applet.show_all()
+
+    def exit_editing(self):
+        self.editing = False
+        self.applet.remove(self.entry)
+        self.applet.add(self.button)
+
+
+def really_get_active_workspace(screen):
+    # This bit is needed because wnck is asynchronous.
+    while Gtk.events_pending():
+        Gtk.main_iteration() 
+    return screen.get_active_workspace()
+
+def applet_factory(applet, iid, data):
+    print "got to factory"
+    WSNameApplet(applet)
+    return True
+
+MatePanelApplet.Applet.factory_main("WsnameAppletFactory", 
+                                    True,
+                                    MatePanelApplet.Applet.__gtype__,
+                                    applet_factory, 
+                                    None)
+
