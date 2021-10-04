d5a3143853f4c65bf28390dab29e9c8af2146b58
Alexandre Muniz <aom@puzzlezapper.com>
2021-10-03 18:51:37 -0700

    Python 2 to 3 changes, but file is not used

--- aligned_window.py
+++ aligned_window.py
@@ -2,10 +2,9 @@
 #Not actually using this currently, because of stuff being broken in gdk 2.x using glib introspection, which isn't even supposed to work, but Mate requires it. 
 #import gtk
 import gi
-gi.require_version("Gtk", "2.0")
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from gi.repository import Gdk
-from gi.repository import MatePanelApplet
 
 class AlignedWindow(Gtk.Window):
 
@@ -17,9 +16,9 @@ class AlignedWindow(Gtk.Window):
 	# Skip the taskbar, and the pager, stick and stay on top
         self.set_decorated(False)
         self.set_type_hint(Gdk.WindowTypeHint.DOCK)
-	self.stick()
+        self.stick()
         self.set_resizable(False)
-        print "got through alignedwindow init"
+        print("got through alignedwindow init")
 
 
     def positionWindow(self):
@@ -28,7 +27,7 @@ class AlignedWindow(Gtk.Window):
         Gdk.flush()
         #print self.window.get_geometry()
         #win = self.get_window()
-        print "got here pw 1"
+        print("got here pw 1")
         #ought to work when we move to gtk3 and the bindings are unbroken
         # or if I port to gnome flashback
         #ourWidth  = (win.get_geometry())[2]
@@ -36,20 +35,20 @@ class AlignedWindow(Gtk.Window):
         (ourWidth, ourHeight) = self.get_size()
         # Get the dimensions/position of the applet
         self.applet.realize()
-	#entryX, entryY = self.widgetToAlignWith.window.get_origin()
+        #entryX, entryY = self.widgetToAlignWith.window.get_origin()
         #entryWidth  = (self.widgetToAlignWith.window.get_geometry())[2]
         #entryHeight = (self.widgetToAlignWith.window.get_geometry())[3]
-        print "got here pw 2"
+        print("got here pw 2")
         #screen = self.applet.get_screen()
         #appletWindow = self.applet.get_window()
         monitor = Gdk.Rectangle(0, 0, 0, 0)
-        print "got here pw 2.1"
+        print("got here pw 2.1")
         appletX = appletY = 0
         (appletX, appletY) = self.applet.window.get_origin()
-        print "got here pw 2.2"
+        print("got here pw 2.2")
         (appletWidth, appletHeight) = self.applet.window.get_size()
         #screen.get_monitor_geometry (screen.get_monitor_at_window (self.applet.window), monitor)
-        print "got_here pw 3"
+        print("got_here pw 3")
         # Get the screen dimensions
         screenHeight = Gdk.screen_height()
         screenWidth = Gdk.screen_width()
